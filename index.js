c115916c9e175cfc508a6eb36938d1760fc737bc
JR Lherm <lherm.jr@gmail.com>
2023-09-01 22:16:23 +0200

    login and signup working

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 // Connect to the DB
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err);
+  });
 
-// HOME ROUTE
-app.get("/", (req, res) => {
-  res.json({ message: "Marvel home page" });
+// Connect to cloudinary
+cloudinary.config({
+  cloud_name: "ddkgnxcot",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const characterRoutes = require("./routes/characters");
@@ -23,9 +39,15 @@ app.use(characterRoutes);
 app.use(comicRoutes);
 app.use(userRoutes);
 
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.json({ message: "Marvel home page" });
+});
+
 app.get("*", (req, res) => {
   res.status(404).json({ message: "This route does not exist" });
 });
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server has started");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,93 @@
 const express = require("express");
 const router = express.Router();
 
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+
+const fileUpload = require("express-fileupload");
+const convertToBase64 = require("../utils/convertToBase64");
+
+const cloudinary = require("cloudinary").v2;
+
+// Import des modeles
+const User = require("../models/Users");
+
+router.post("/signup", fileUpload(), async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      res.status(409).json({ message: "Email already has an account." });
+    } else if (req.body.email && req.body.password && req.body.username) {
+      const token = uid2(64);
+      const salt = uid2(64);
+      const hash = SHA256(req.body.password + salt).toString(encBase64);
+
+      const newUser = new User({
+        email: req.body.email,
+        token: token,
+        hash: hash,
+        salt: salt,
+        account: {
+          username: req.body.username,
+        },
+        acceptCGU: req.body.acceptCGU,
+      });
+
+      if (req.files?.avatar) {
+        const result = await cloudinary.uploader.upload(
+          convertToBase64(req.files.avatar),
+          {
+            folder: `api/marvel/users/${newUser._id}`,
+            public_id: "avatar",
+          }
+        );
+        newUser.account.avatar = result;
+      }
+
+      await newUser.save();
+      console.log(newUser);
+      res.status(201).json({
+        _id: newUser._id,
+        email: newUser.email,
+        token: newUser.token,
+        account: newUser.account,
+      });
+    } else {
+      res.status(400).json({ message: "Missing parameters" });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  console.log("Received email: ", req.body.email);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    console.log("Found user: ", user);
+
+    if (user) {
+      if (
+        SHA256(req.body.password + user.salt).toString(encBase64) === user.hash
+      ) {
+        res.status(200).json({
+          _id: user._id,
+          token: user.token,
+          account: user.account,
+        });
+      } else {
+        res.status(401).json({ error: "Unauthorized" });
+      }
+    } else {
+      res.status(400).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
